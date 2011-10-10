0c518841355e74fffe50bbbdcfd9fa6cfa233b56
Leo Jweda <LeoJweda@gmail.com>
2011-10-10 19:46:37 -0400

    Initial commit

---
+++ README.markdown
@@ -0,0 +1,7 @@
+# Mypothesis
+
+Mypothesis is a web application that allows users to share hypotheses they have and allows others to contribute evidences for and against that hypothesis.
+
+Mypothesis is created by [Leo Jweda](http://www.LeoJweda.com/). 
+
+This application will be hosted on [Heroku](http://www.heroku.com/).
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Mypothesis::Application.config.secret_token = '41e8e6f3de61734aca343de0056d4810175e0333ca75b40c2088546436627b5b47d8c5b0ece626b917affdd581f5b5ae3b0f90b252f043a97dd4791cc55fd1c6'
