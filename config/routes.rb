913c4dcfaa5f587e6038aa683e54320c83775dcc
Leo Jweda <LeoJweda@gmail.com>
2011-10-18 13:30:27 -0400

    Added a Pages controller for #1

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,12 @@
 Mypothesis::Application.routes.draw do
+  get "pages/home"
+
+  get "pages/about"
+
+  get "pages/faq"
+
+  get "pages/contact"
+
   # The priority is based upon order of creation:
   # first created -> highest priority.
 
