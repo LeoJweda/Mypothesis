d3acc2576ded8a0b4d6700758f038fd2f8312fe3
Leo Jweda <LeoJweda@gmail.com>
2011-10-19 14:30:57 -0400

    Mapped pages to corresponding addresses for #1

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,9 @@
 Mypothesis::Application.routes.draw do
-  get "pages/home"
+  match "/about", :to => 'pages#about'
 
-  get "pages/about"
+  match "/faq", :to => 'pages#faq'
 
-  get "pages/faq"
-
-  get "pages/contact"
+  match "/contact", :to => 'pages#contact'
 
   # The priority is based upon order of creation:
   # first created -> highest priority.
@@ -56,7 +54,7 @@ Mypothesis::Application.routes.draw do
 
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
-  # root :to => 'welcome#index'
+  root :to => 'pages#home'
 
   # See how all your routes lay out with "rake routes"
 
