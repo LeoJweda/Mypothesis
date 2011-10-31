4f7baf9da00bbe9d1ef95ac3d42ba09d22c6856e
Leo Jweda <LeoJweda@gmail.com>
2011-10-31 02:10:47 -0400

    Configured devise for #3

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -57,4 +57,6 @@ Mypothesis::Application.configure do
 
   # Send deprecation notices to registered listeners
   config.active_support.deprecation = :notify
+  
+  config.action_mailer.default_url_options = { :host => 'www.mypothesis.com' }
 end
