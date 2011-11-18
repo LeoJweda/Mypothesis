f07e5cdf4105a78f2436b283bf20d0fab5a31dd5
Leo Jweda <LeoJweda@gmail.com>
2011-11-18 12:19:07 -0500

    Refactored a bit. Closes #8

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,15 +1,5 @@
 class PagesController < ApplicationController
   def home
-    @hypotheses = []
-    if !params[:subject] || params[:subject] == 'All'
-      @hypotheses = Hypothesis.all
-    else
-      @hypotheses = Hypothesis.find(:all, :conditions => ["subject = ?", params[:subject]])
-    end
-    
-    if params[:order] == 'best'
-      @hypotheses.sort!{|a, b| b.score <=> a.score}
-    end
   end
 
   def about
