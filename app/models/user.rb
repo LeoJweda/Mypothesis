61fc8c17bd8c5cedb30b77bf8e700521894a1fa5
Leo Jweda <LeoJweda@gmail.com>
2011-12-21 23:31:36 -0500

    Added first name, last name, organization, and title to user profiles for #9

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,12 +5,18 @@ class User < ActiveRecord::Base
          :recoverable, :rememberable, :trackable, :validatable, :omniauthable
 
   # Setup accessible (or protected) attributes for your model
-  attr_accessible :name, :email, :password, :password_confirmation, :remember_me
+  attr_accessible :first_name, :last_name, :organization, :title, :email, :password, :password_confirmation, :remember_me
   
   has_many :hypotheses
   has_many :evidences
   
+  validates :first_name, :last_name, :organization, :title, :presence => true
+  
   acts_as_voter
   
   ROLES = %w[admin moderator basic banned]
+  
+  def full_name
+    "#{first_name} #{last_name}"
+  end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20111218035557) do
+ActiveRecord::Schema.define(:version => 20111222040740) do
 
   create_table "evidences", :force => true do |t|
     t.string   "title"
@@ -55,8 +55,12 @@ ActiveRecord::Schema.define(:version => 20111218035557) do
     t.string   "last_sign_in_ip"
     t.datetime "created_at"
     t.datetime "updated_at"
-    t.string   "name"
     t.string   "role"
+    t.string   "name"
+    t.string   "first_name"
+    t.string   "last_name"
+    t.string   "organization"
+    t.string   "title"
   end
 
   add_index "users", ["email"], :name => "index_users_on_email", :unique => true
