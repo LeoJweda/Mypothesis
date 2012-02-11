b92009e9cbd062e4b1c06e3c5f3d004bac80f984
Leo Jweda <LeoJweda@gmail.com>
2012-02-11 03:46:31 -0500

    Changed most of the site to use Bootstrap for #20

--- app/helpers/evidences_helper.rb
+++ app/helpers/evidences_helper.rb
@@ -1,5 +1,9 @@
 module EvidencesHelper
-  def vote_link_to(evidence, direction, visible)
-    link_to "#{direction == 'up' ? 'Up' : 'Down'} Vote", ((direction == 'up') ? up_vote_hypothesis_evidence_path(evidence.hypothesis, evidence) : down_vote_hypothesis_evidence_path(evidence.hypothesis, evidence)), :method => :put, :remote => true, :class => "#{direction == 'up' ? 'upvote' : 'downvote'} #{visible ? '' : 'ui-helper-hidden-accessible'}"
+  def up_vote_link_to(evidence)
+    link_to content_tag(:i, '', :class => "icon-chevron-up#{(!current_user.voted_as_when_voted_for(evidence) || current_user.voted_as_when_voted_for(evidence) == nil) ? '' : ' icon-white'}"), up_vote_hypothesis_evidence_path(evidence.hypothesis, evidence), :method => :put, :remote => true, :class => "btn#{(!current_user.voted_as_when_voted_for(evidence) || current_user.voted_as_when_voted_for(evidence) == nil) ? '' : ' btn-success'} upvote"
+  end
+  
+  def down_vote_link_to(evidence)
+    link_to content_tag(:i, '', :class => "icon-chevron-down#{(current_user.voted_as_when_voted_for(evidence) || current_user.voted_as_when_voted_for(evidence) == nil) ? '' : ' icon-white'}"), down_vote_hypothesis_evidence_path(evidence.hypothesis, evidence), :method => :put, :remote => true, :class => "btn#{(current_user.voted_as_when_voted_for(evidence) || current_user.voted_as_when_voted_for(evidence) == nil) ? '' : ' btn-danger'} downvote"
   end
 end
